c7bb2dcb00fe04b7e546604f05d84d33d6d12541
Adwiti789 <adwiti1975@gmail.com>
2018-04-22 10:16:25 -0700

    Skeleton of Paging

--- src/main/java/com/arun/dao/PaginationDaoImpl.java
+++ src/main/java/com/arun/dao/PaginationDaoImpl.java
@@ -1,12 +1,14 @@
 package com.arun.dao;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by Adwiti on 4/22/2018.
  */
+@Repository
 public class PaginationDaoImpl implements PaginationDao {
     @Override
     public List<String> getAllEmployee(Pageable pageable, String name) {
--- src/main/java/com/arun/service/PaginationServiceImpl.java
+++ src/main/java/com/arun/service/PaginationServiceImpl.java
@@ -3,12 +3,14 @@ package com.arun.service;
 import com.arun.dao.PaginationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by Adwiti on 4/22/2018.
  */
+@Service
 public class PaginationServiceImpl implements PaginationService {
     @Autowired
     private PaginationDao paginationDao;
