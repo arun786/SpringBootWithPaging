10e8719c21ac5c43b75518b605f3bd73f73217d7
Adwiti789 <adwiti1975@gmail.com>
2018-04-22 09:19:39 -0700

    First commit

---
+++ src/main/java/com/arun/SpringBootWihPagingApplication.java
@@ -0,0 +1,12 @@
+package com.arun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWihPagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWihPagingApplication.class, args);
+	}
+}
