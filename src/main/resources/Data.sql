c1c039c5a284241e194e34ef22647d44e400113f
Adwiti789 <adwiti1975@gmail.com>
2018-04-22 23:47:01 -0700

    Pagination

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -6,7 +6,7 @@ std varchar(255) not null,
 address varchar(255) not null);
 
 insert into Student values (1, 'arun',12,'x','scottsdale');
-insert into Student values (2, 'pushpa',10,'x','scottsdale');
-insert into Student values (3, 'adwiti',8,'x','scottsdale');
-insert into Student values (4, 'sachin',32,'x','scottsdale');
-insert into Student values (5, 'dhoni',15,'x','scottsdale');
+insert into Student values (2, 'arun',10,'x','scottsdale');
+insert into Student values (3, 'arun',8,'x','scottsdale');
+insert into Student values (4, 'arun',32,'x','scottsdale');
+insert into Student values (5, 'arun',15,'x','scottsdale');
